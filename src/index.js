2f1c03cab33ce3e630355d8f1844c5b6588c337c
yitz <feigenbaumsfish@gmail.com>
2025-09-09 17:18:52 -0500

    updated import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './componants/App.jsx';
+import App from './components/App.jsx';
 
 // Import CSS styles
 import './styles/main.css';
